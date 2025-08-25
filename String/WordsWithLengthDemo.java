d58eccd4b0a469e173b89033632febf1e33b3619
blacksmith011 <aryanceo888@gmail.com>
2025-08-25 20:24:07 +0530

    Create WordsWithLengthDemo.java

---
+++ String/WordsWithLengthDemo.java
@@ -0,0 +1,59 @@
+import java.util.Scanner;
+
+public class WordsWithLengthDemo {
+    public static int getLength(String text) {
+        int count = 0;
+        try {
+            while (true) {
+                text.charAt(count);
+                count++;
+            }
+        } catch (StringIndexOutOfBoundsException e) {}
+        return count;
+    }
+
+    public static String[] manualSplit(String text) {
+        int length = getLength(text);
+        int spaceCount = 0;
+
+        for (int i = 0; i < length; i++) {
+            if (text.charAt(i) == ' ') spaceCount++;
+        }
+
+        String[] words = new String[spaceCount + 1];
+        int start = 0, index = 0;
+
+        for (int i = 0; i < length; i++) {
+            if (text.charAt(i) == ' ') {
+                words[index++] = text.substring(start, i);
+                start = i + 1;
+            }
+        }
+        words[index] = text.substring(start, length);
+
+        return words;
+    }
+
+    public static String[][] getWordsWithLength(String[] words) {
+        String[][] result = new String[words.length][2];
+        for (int i = 0; i < words.length; i++) {
+            result[i][0] = words[i];
+            result[i][1] = String.valueOf(getLength(words[i]));
+        }
+        return result;
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter text: ");
+        String text = sc.nextLine();
+
+        String[] words = manualSplit(text);
+        String[][] table = getWordsWithLength(words);
+
+        System.out.println("\nWord\tLength");
+        for (String[] row : table) {
+            System.out.println(row[0] + "\t" + row[1]);
+        }
+    }
+}
