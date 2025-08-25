118aa753767f5b0be7491cf76bff86ad0563c92b
blacksmith011 <aryanceo888@gmail.com>
2025-08-25 21:08:21 +0530

    Create TrigonometricFunctions.java

---
+++ Methods/TrigonometricFunctions.java
@@ -0,0 +1,31 @@
+import java.util.Scanner;
+
+public class TrigonometricFunctions {
+
+    // Method to calculate sin, cos, tan
+    public static double[] calculateTrigonometricFunctions(double angle) {
+        double radians = Math.toRadians(angle); // Convert degrees to radians
+        double sine = Math.sin(radians);
+        double cosine = Math.cos(radians);
+        double tangent = Math.tan(radians);
+
+        return new double[]{sine, cosine, tangent};
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        // User input
+        System.out.print("Enter angle in degrees: ");
+        double angle = sc.nextDouble();
+
+        // Get results
+        double[] results = calculateTrigonometricFunctions(angle);
+
+        System.out.println("Sine(" + angle + ") = " + results[0]);
+        System.out.println("Cosine(" + angle + ") = " + results[1]);
+        System.out.println("Tangent(" + angle + ") = " + results[2]);
+
+        sc.close();
+    }
+}
