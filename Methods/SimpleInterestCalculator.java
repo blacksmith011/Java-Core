a32623d20da7ca0913f04126fdfc368d19f6d161
blacksmith011 <aryanceo888@gmail.com>
2025-08-25 21:03:17 +0530

    Create SimpleInterestCalculator.java

---
+++ Methods/SimpleInterestCalculator.java
@@ -0,0 +1,34 @@
+import java.util.Scanner;
+
+public class SimpleInterestCalculator {
+
+    // Method to calculate SI
+    public static double calculateSimpleInterest(double principal, double rate, double time) {
+        return (principal * rate * time) / 100;
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        // Taking inputs
+        System.out.print("Enter Principal: ");
+        double principal = sc.nextDouble();
+
+        System.out.print("Enter Rate of Interest: ");
+        double rate = sc.nextDouble();
+
+        System.out.print("Enter Time (in years): ");
+        double time = sc.nextDouble();
+
+        // Calling method
+        double si = calculateSimpleInterest(principal, rate, time);
+
+        // Output
+        System.out.println("The Simple Interest is " + si +
+                " for Principal " + principal +
+                ", Rate of Interest " + rate +
+                " and Time " + time);
+        
+        sc.close();
+    }
+}
