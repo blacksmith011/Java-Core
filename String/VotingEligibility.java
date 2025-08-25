819eb56efdb60281e51c41ccc4bfbe0724b15447
blacksmith011 <aryanceo888@gmail.com>
2025-08-25 20:29:25 +0530

    Create VotingEligibility.java

---
+++ String/VotingEligibility.java
@@ -0,0 +1,36 @@
+import java.util.Scanner;
+
+public class VotingEligibility {
+    public static int[] generateAges(int n) {
+        int[] ages = new int[n];
+        for (int i = 0; i < n; i++) {
+            ages[i] = (int)(Math.random() * 90);
+        }
+        return ages;
+    }
+
+    public static String[][] checkEligibility(int[] ages) {
+        String[][] result = new String[ages.length][2];
+        for (int i = 0; i < ages.length; i++) {
+            result[i][0] = String.valueOf(ages[i]);
+            result[i][1] = (ages[i] >= 18) ? "true" : "false";
+        }
+        return result;
+    }
+
+    public static void displayTable(String[][] data) {
+        System.out.println("Age\tCan Vote");
+        for (String[] row : data) {
+            System.out.println(row[0] + "\t" + row[1]);
+        }
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter number of students: ");
+        int n = sc.nextInt();
+        int[] ages = generateAges(n);
+        String[][] result = checkEligibility(ages);
+        displayTable(result);
+    }
+}
