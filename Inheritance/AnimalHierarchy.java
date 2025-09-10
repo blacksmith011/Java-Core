9df2fd51fd79737f53eecc4d95729908958b7a37
blacksmith011 <aryanceo888@gmail.com>
2025-09-10 11:16:51 +0530

    Create AnimalHierarchy.java

---
+++ Inheritance/AnimalHierarchy.java
@@ -0,0 +1,67 @@
+// Superclass
+class Animal {
+    String name;
+    int age;
+
+    // Constructor
+    Animal(String name, int age) {
+        this.name = name;
+        this.age = age;
+    }
+
+    // Method to be overridden
+    void makeSound() {
+        System.out.println("Animal makes a sound");
+    }
+}
+
+// Subclass Dog
+class Dog extends Animal {
+    Dog(String name, int age) {
+        super(name, age);
+    }
+
+    @Override
+    void makeSound() {
+        System.out.println(name + " barks! 🐶");
+    }
+}
+
+// Subclass Cat
+class Cat extends Animal {
+    Cat(String name, int age) {
+        super(name, age);
+    }
+
+    @Override
+    void makeSound() {
+        System.out.println(name + " meows! 🐱");
+    }
+}
+
+// Subclass Bird
+class Bird extends Animal {
+    Bird(String name, int age) {
+        super(name, age);
+    }
+
+    @Override
+    void makeSound() {
+        System.out.println(name + " chirps! 🐦");
+    }
+}
+
+// Main class
+public class AnimalHierarchy {
+    public static void main(String[] args) {
+        // Polymorphism: same reference type, different object types
+        Animal a1 = new Dog("Tommy", 3);
+        Animal a2 = new Cat("Kitty", 2);
+        Animal a3 = new Bird("Tweety", 1);
+
+        // Method overriding in action
+        a1.makeSound();  // Tommy barks! 🐶
+        a2.makeSound();  // Kitty meows! 🐱
+        a3.makeSound();  // Tweety chirps! 🐦
+    }
+}
